bcab3de8e5f940ccd8b2807f9bfbeb3ea795c2e7
eliscamastenbroek <elisca-m@hotmail.com>
2023-07-19 17:35:52 +0200

    Update and rename Simulation1.R to Perform_Simulation1.R

--- Simulation_and_Analyses/Perform_Simulation1.R
+++ Simulation_and_Analyses/Perform_Simulation1.R
@@ -1,4 +1,4 @@
-##################################### Simulation1.R ##############################################
+################################# Perform_Simulation1.R ##########################################
 ## This file contains the code that is required to perform the simulation study without missing ##
 ## covariates as described in Chapter 4. The file is divided into three parts:                  ##
 ##   1. Function 'simulate_data' to simulate data for this simulation study.                    ##
