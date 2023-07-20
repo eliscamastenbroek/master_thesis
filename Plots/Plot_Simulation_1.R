20560491f704c45a51fc4c4cd1fa8ba3441520db
eliscamastenbroek <elisca-m@hotmail.com>
2023-07-20 10:00:54 +0200

    Update Plot_Simulation_1.R

--- Plots/Plot_Simulation_1.R
+++ Plots/Plot_Simulation_1.R
@@ -1,16 +1,19 @@
-#################################### Plot_Simulation_1.R ######################################### 
+################################### Plot_Simulation_1.R ########################################## 
 ## This file contains the code that is required to create the plots for the simulation study    ##
-## in Chapter 4. To create most plots, the following objects are required to be in the global   ##
-## environment:                                                                                 ##
+## in Chapter 4.                                                                                ##
+##                                                                                              ##
+## To create most plots, the following objects are required:                                    ##
 ##    - plot_df (data.frame): Data frame with results to plot the PPEs, the entropy R2, and     ##
 ##      the mean summed bias                                                                    ##
 ##    - plot_ME (data.frame): Data frame with results to plot the expected value of the MEPEs   ##
 ##    - plot_rmse_ME (data.frame): Data frame with results to plot the RMSE of the MEPEs        ##
-## These objects can be created by:                                                             ##
-##    1) Performing the simulation study in the file 'Perform_Simulation1.R' and preparing the  ##
+##                                                                                              ##
+## These objects can be obtained by either:                                                     ##
+##    1) Performing the simulation study in the file 'Perform_Simulation_1.R' and preparing the ##
 ##       data for plotting using the file 'Prepare_Simulation_Results_for_Plotting.R', or:      ##
 ##    2) Loading the RData in the file 'Simulation1_Reduced.RData'.                             ##
-## Note that to create the heatmaps, the following objects are also required:                   ##
+##                                                                                              ##
+## Note that to create the heatmaps, the following objects are required in addition:            ##
 ##    - LC_models (list): List containing the (detailed) model results for LC analysis          ##
 ##    - LCT_models (list): List containing the (detailed) model results for LCT analysis        ##
 ##    - treeMILC_models (list): List containing the (detailed) model results for tree-MILC      ## 
@@ -147,28 +150,22 @@ plot_df %>% ggplot(aes(x=indicator, y=diag, fill=type)) +
 
 # Simulation study 1
 ind <- 2:4
-cov_problem <- "null"
-
-# Simulation study 2
-ind <- 2:3
-cov_problem <- c("baanduur", "SBIgroep", "")
 
 for (j in c(1000, 10000)) {
-  for (m in cov_problem) {
     for (k in ind) {
       if (k == 2) {
         cov_ok <- "q"
       } else {
         cov_ok <- NULL
       }
+      
       # Plot bias and variance for 10%, 20%, and 30% ME
-      bias1 <- get_ME_heatmap(LC_models, LCT_models, treeMILC_models, LC_results, 1, k, cov_ok, cov_problem, j)
-      variance1 <- get_ME_heatmap(LC_models, LCT_models, treeMILC_models, LC_results, 2, k, cov_ok, cov_problem, j)
+      bias1 <- get_ME_heatmap(LC_models, LCT_models, treeMILC_models, LC_results, 1, k, cov_ok, NULL, j)
+      variance1 <- get_ME_heatmap(LC_models, LCT_models, treeMILC_models, LC_results, 2, k, cov_ok, NULL, j)
       
       # Plot bias and variance for realistic 7% ME
-      bias2 <- get_ME_heatmap_realistic(LC_models, LCT_models, treeMILC_models, LC_results, 1, k, cov_ok, cov_problem, j)
-      variance2 <- get_ME_heatmap_realistic(LC_models, LCT_models, treeMILC_models, LC_results, 1, k, cov_ok, cov_problem, j)
-
+      bias2 <- get_ME_heatmap_realistic(LC_models, LCT_models, treeMILC_models, LC_results, 1, k, cov_ok, NULL, j)
+      variance2 <- get_ME_heatmap_realistic(LC_models, LCT_models, treeMILC_models, LC_results, 1, k, cov_ok, NULL, j)
+   
     }  
-  }
 }
